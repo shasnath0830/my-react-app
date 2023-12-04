9dde392477804ceb2b21d9da17583d94fe20c2f3
Sadekin Hasnath <shasnath2@student.gsu.edu>
2023-12-03 19:27:02 -0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/my-react-app/', // Adjust this if your repository name is different
+  base: '/my-react-app/',
   plugins: [react()],
   build: {
     outDir: 'build',
